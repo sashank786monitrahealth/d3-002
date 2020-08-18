99640e71c33320a5c845beae9e01d95b38f8526d
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-08-18 07:54:16 +0530

    simple dev setup

---
+++ 003-quick-dev-setup/src/main.js
@@ -0,0 +1,6 @@
+import * as d3 from 'd3'
+
+
+d3.select('h1').style('background-color','gold');
+d3.select('h2').style('background-color','blue');
+d3.select('h3').style('background-color','chartreuse');
